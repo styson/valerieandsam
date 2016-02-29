9f55740151f86c0c7dd3a864892d5e36f432e4e0
Sam Tyson <styson@gmail.com>
2016-02-29 17:37:51 -0600

    updates

--- xhtml/header.php
+++
@@ -1,3 +0,0 @@
-  <div id="header">
-		Smelly Cat Coffeehouse
-	</div>
--- xhtml/menu.php
+++
@@ -1,8 +0,0 @@
-  <div id="navigation">
-  	<ul>
-			<li class=<?php if($page == 0) { echo "selMenu"; } else { echo "menu"; } ?>><a href="./index.php"><span>Home</span></a></li>
-			<li class=<?php if($page == 1) { echo "selMenu"; } else { echo "menu"; } ?>><a href="./links.php"><span>Links</span></a></li>
-			<li class=<?php if($page == 2) { echo "selMenu"; } else { echo "menu"; } ?>><a href="./products.php"><span>Products</span></a></li>
-			<li class=<?php if($page == 3) { echo "selMenu"; } else { echo "menu"; } ?>><a href="./contact.php"><span>Contact Us</span></a></li>
-		</ul>
-	</div>
